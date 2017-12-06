7a08c4d05f99d0cd5e96c735ba0043ddfefda341
Jax <coffeejmg@gmail.com>
2017-12-06 09:14:25 -0500

    navigation transition upon scroll

--- script/rotate.js
+++ script/rotate.js
@@ -14,3 +14,27 @@ function loop(i) {
 loop(0);
 
 });
+
+// navigation transition
+$(window).scroll(function() {
+// 100 = The point you would like to fade the nav in.
+  
+	if ($(window).scrollTop() > 100 ){
+    
+ 		$('.navbar .container .bg').addClass('show');
+    
+  } else {
+    
+    $('.navbar .container .bg').removeClass('show');
+    
+ 	};   	
+});
+
+$('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
+
